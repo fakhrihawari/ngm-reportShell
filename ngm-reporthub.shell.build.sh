39b77a8c526dcbabcb0deacf16c7ad457c3a2332
pfitzpaddy <pfitzgerald@immap.org>
2018-03-28 15:36:19 +0300

    Updated install script

--- ngm-reporthub.shell.build.sh
+++ ngm-reporthub.shell.build.sh
@@ -119,7 +119,7 @@ sudo -u postgres psql -c "CREATE DATABASE immap_afg WITH OWNER ngmadmin;"
 sudo -u postgres psql -d immap_afg -c "CREATE EXTENSION postgis;"
 
 # add pgpass for command line access
-echo -e "localhost:5432:immap_afg:ngmadmin:ngmadmin" | sudo tee ~/.pgpass
+echo -e "192.168.33.16:5432:immap_afg:ngmadmin:ngmadmin" | sudo tee ~/.pgpass
 sudo chmod 0600 ~/.pgpass
 
 # udpate listen_addresses
@@ -155,38 +155,59 @@ cd /home/ubuntu/nginx/www
 sudo git clone https://github.com/pfitzpaddy/ngm-reportPrint.git
 
 
-
 ####################################################### ngm-ReportHub
-cd /home/ubuntu/nginx/www
 
-# UPDATE TO YOUR FORKED REPO!
-# https://help.github.com/articles/changing-a-remote-s-url/
-sudo git clone https://github.com/<your.fork>/ngm-reportHub.git
+## UPDATE TO YOUR FORKED REPO!
+## https://help.github.com/articles/changing-a-remote-s-url/
+# sudo git clone https://github.com/<your.fork>/ngm-reportHub.git
 
-# build Hub app
+
+# build HUB app BOWER and NODE
 cd /home/ubuntu/nginx/www/ngm-reportHub
-sudo npm install --allow-root
-# sudo bower ( lib dependices conflict )
-wget https://www.dropbox.com/s/5obb3lqo9el8my2/bower_components.zip?dl=1
-unzip bower_components.zip?dl=1
-sudo rm bower_components.zip\?dl\=1
-sudo gulp
+
+## ONLINE
+## node_modules
+# wget https://www.dropbox.com/s/ie8l41wgvwe0xke/node_modules.zip?dl=1
+# unzip node_modules.zip?dl=1
+# sudo rm node_modules.zip\?dl\=1
+
+## bower_components
+# wget https://www.dropbox.com/s/5obb3lqo9el8my2/bower_components.zip?dl=1
+# unzip bower_components.zip?dl=1
+# sudo rm bower_components.zip\?dl\=1
+
+## LOCAL
+# node_modules
+sudo cp ../data/config/reportHub/node_modules.zip /home/ubuntu/nginx/www/ngm-reportHub
+unzip node_modules.zip
+sudo rm node_modules.zip
+
+# bower_components
+sudo cp ../data/config/reportHub/bower_components.zip /home/ubuntu/nginx/www/ngm-reportHub
+unzip bower_components.zip
+sudo rm bower_components.zip
 
 
 
 ####################################################### ngm-ReportEngine
-cd /home/ubuntu/nginx/www
 
-# UPDATE TO YOUR FORKED REPO!
-# https://help.github.com/articles/changing-a-remote-s-url/
-sudo git clone https://github.com/<your.fork>/ngm-reportEngine.git
+## UPDATE TO YOUR FORKED REPO!
+## https://help.github.com/articles/changing-a-remote-s-url/
+# sudo git clone https://github.com/<your.fork>/ngm-reportEngine.git
 
-# build sails app
+## build ENGINE app NODE
 cd /home/ubuntu/nginx/www/ngm-reportEngine
-# sudo bower ( lib dependices conflict )
-wget https://www.dropbox.com/s/ie8l41wgvwe0xke/node_modules.zip?dl=1
-unzip node_modules.zip?dl=1
-sudo rm node_modules.zip\?dl\=1
+
+## ONLINE
+# wget https://www.dropbox.com/s/ie8l41wgvwe0xke/node_modules.zip?dl=1
+# unzip node_modules.zip?dl=1
+# sudo rm node_modules.zip\?dl\=1
+
+## LOCAL
+# node_modules
+sudo cp ../data/config/reportEngine/node_modules.zip /home/ubuntu/nginx/www/ngm-reportEngine
+unzip node_modules.zip
+sudo rm node_modules.zip
 
 
 # connection config sails li
@@ -223,6 +244,15 @@ module.exports = {
 			// user: 'username',
 			// password: 'password',
 			database: 'ngmEpr',
+			schema: false
+		},
+		ngmEthCtcServer: {
+			adapter: 'sails-mongo',
+			host: 'localhost',
+			port: 27017,
+			// user: 'username',
+			// password: 'password',
+			database: 'ngmEthCtc',
 			schema: false
 		},
 		ngmPostgreServer: {
@@ -312,7 +342,7 @@ server {
 
 }" | sudo tee /etc/nginx/sites-available/default
 # symb link
-sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
+# sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 # reload configuration
 sudo service nginx restart
 
@@ -332,22 +362,42 @@ sudo gunzip -c /home/ubuntu/data/postgres/immap_afg.gz | psql -U ngmadmin -h loc
 
 
 # restore mongodb scripts
+mongorestore --drop -d ngmEpr /home/ubuntu/data/mongo/ngmEpr
 mongorestore --drop -d ngmReportHub /home/ubuntu/data/mongo/ngmReportHub
 mongorestore --drop -d ngmHealthCluster /home/ubuntu/data/mongo/ngmHealthCluster
-mongorestore --drop -d ngmEpr /home/ubuntu/data/mongo/ngmEpr
+mongorestore --drop -d ngmEthCtc /home/ubuntu/data/mongo/ngmEthCtc
 
 
-# import collection
+# # import collection
 mongoimport -d ngmHealthCluster -c activities --drop --headerline --type csv --file /home/ubuntu/data/csv/activities.csv
-mongoimport -d ngmHealthCluster -c donors --drop --headerline --type csv --file /home/ubuntu/data/csv/donors.csv
-mongoimport -d ngmHealthCluster -c stockitems --drop --headerline --type csv --file /home/ubuntu/data/csv/stockitems.csv
-mongoimport -d ngmReportHub -c organizations --drop --headerline --type csv --file /home/ubuntu/data/csv/organizations.csv
+# mongoimport -d ngmHealthCluster -c donors --drop --headerline --type csv --file /home/ubuntu/data/csv/donors.csv
+# mongoimport -d ngmHealthCluster -c stockitems --drop --headerline --type csv --file /home/ubuntu/data/csv/stockitems.csv
+# mongoimport -d ngmReportHub -c organizations --drop --headerline --type csv --file /home/ubuntu/data/csv/organizations.csv
 
-# # ethiopian health facilities
-# mongoimport -d ngmReportHub -c admin3facilities --drop --file /home/ubuntu/data/json/admin3facilities.json
-# # export CSV
-# mongoimport -d ngmHealthCluster -c reporthub_indicators_hct --drop --jsonArray --file /home/ubuntu/data/json/reporthub_indicators_hct.json
-# mongoexport --db ngmHealthCluster --collection reporthub_indicators_hct --type=csv --fields id,title,districts,white_area_districts,idp_districts,wa_idp_districts,bphs_districts,bphs_services,hc_districts,hc_services,categories,bphs_data,hc_data --out /home/ubuntu/data/json/reporthub_indicators_hct.csv
+# # import admins
+# mongoimport -d ngmReportHub -c admin1 --type json --file /home/ubuntu/data/json/sy_admin_1.json --jsonArray
+# mongoimport -d ngmReportHub -c admin2 --type json --file /home/ubuntu/data/json/sy_admin_2.json --jsonArray
+# mongoimport -d ngmReportHub -c admin3 --type json --file /home/ubuntu/data/json/sy_admin_3.json --jsonArray
+
+# # # ethiopian health facilities
+# # mongoimport -d ngmReportHub -c admin3facilities --drop --file /home/ubuntu/data/json/admin3facilities.json
+
+# # # export CSV
+# # mongoimport -d ngmHealthCluster -c reporthub_indicators_hct --drop --jsonArray --file /home/ubuntu/data/json/reporthub_indicators_hct.json
+# mongoexport --db ngmReportHub  --collection user   -q "{ 'admin0pcode': 'ET' }" --type=csv --fields id,admin0pcode,cluster,organization,username,name,position,phone,email,skype,visits,createdAt,updatedAt --out /home/ubuntu/data/csv/cdc/et_users.csv
+
+# # trainings
+# mongoexport --db ngmHealthCluster  --collection trainingparticipants  --type=csv --fields adminRpcode,adminRname,admin0pcode,admin0name,organization_id,organization_tag,organization,implementing_partners,cluster_id,cluster,name,position,phone,email,username,project_id,project_acbar_partner,project_hrp_code,project_code,project_status,project_title,project_description,project_start_date,project_end_date,project_budget,project_budget_currency,mpc_purpose,mpc_purpose_cluster_id,inter_cluster_activities,project_donor,strategic_objectives,report_id,report_active,report_status,report_month,report_year,report_submitted,reporting_period,reporting_due_date,training_id,training_title,training_topics,training_start_date,training_end_date,training_days_number,training_conducted_by,training_supported_by,trainee_affiliation_id,trainee_affiliation_name,trainee_health_worker_id,trainee_health_worker_name,trainee_men,trainee_women,location_id,admin1pcode,admin1name,admin2pcode,admin2name,admin3pcode,admin3name,facility_id,facility_class,facility_status,facility_implementation_id,facility_implementation_name,facility_type_id,facility_type_name,facility_name,facility_hub_id,facility_hub_name,conflict,admin1lng,admin1lat,admin2lng,admin2lat,admin3lat,facility_lng,facility_lat --out /home/ubuntu/data/csv/trainings/trainingparticipants.csv
+
+# mongoexport --db ngmReportHub  --collection admin1 -q '{ admin0pcode: "ET" }' --type=csv --fields admin1pcode,admin1name --out /home/ubuntu/data/csv/ethiopia/admin1Et.csv
+# mongoexport --db ngmReportHub  --collection admin2 -q '{ admin0pcode: "ET" }' --type=csv --fields admin2pcode,admin2name,admin1pcode --out /home/ubuntu/data/csv/ethiopia/admin2Et.csv
+# mongoexport --db ngmReportHub  --collection admin3 -q '{ admin0pcode: "ET" }' --type=csv --fields admin3pcode,admin3name,admin1pcode,admin2pcode, --out /home/ubuntu/data/csv/ethiopia/admin3Et.csv
+# mongoexport --db ngmReportHub  --collection admin3facilities -q '{ admin0pcode: "ET" }' --type=csv --fields admin1pcode,admin1name,admin2pcode,admin2name,admin3pcode,admin3name,facility_id,facility_type,facility_name --out /home/ubuntu/data/csv/ethiopia/facilitiesEt.csv
+
+
+## start the ENGINE
+cd /home/ubuntu/nginx/www/ngm-reportEngine
+sudo sails lift
 
 
 
